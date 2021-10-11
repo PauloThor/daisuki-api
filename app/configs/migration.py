90bc532d1ff5de607e57bd4083ea0630f067d110
Matheus Paiva <matheeus_paiva@hotmail.com>
2021-10-11 15:48:25 -0300

    Add UserModel

--- app/configs/migration.py
+++ app/configs/migration.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 def init_app(app: Flask):
 
     from app.models.anime_model import AnimeModel
-
+    from app.models.user_model import UserModel
+    
     Migrate(app, app.db)
 
