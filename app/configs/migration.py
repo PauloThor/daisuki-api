136436c0d41be23f4a12288f5ab75fd25b6acb53
Matheus Paiva <matheeus_paiva@hotmail.com>
2021-10-11 13:42:47 -0300

    feat: AnimeModel

--- app/configs/migration.py
+++ app/configs/migration.py
@@ -2,5 +2,8 @@ from flask import Flask
 from flask_migrate import Migrate
 
 def init_app(app: Flask):
+
+    from app.models.anime_model import AnimeModel
+
     Migrate(app, app.db)
 
---
+++ app/models/anime_model.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from datetime import datetime
+from app.configs.database import db
+from sqlalchemy import Column, String, Integer, Boolean, DateTime
+ 
+
+@dataclass
+class AnimeModel(db.Model):
+ 
+   id: int
+   name: str
+   synopsis: str
+   image_url: str
+   total_episodes: int
+   is_movie: bool
+   is_dubbed: bool
+   is_completed: bool
+   created_at: datetime
+ 
+   __tablename__ = 'animes'
+ 
+  
+   id = Column(Integer, primary_key=True)
+   name = Column(String(255), nullable=True, unique=True)
+   synopsis = Column(String(1023), nullable=False)
+   image_url = Column(String(255), nullable=False)
+   total_episodes = Column(Integer, nullable=False)
+   is_movie = Column(Boolean, nullable=False)
+   is_dubbed = Column(Boolean, nullable=False)
+   is_completed = Column(Boolean, nullable=False)
+   created_at = Column(DateTime, nullable=False)
+
+   
