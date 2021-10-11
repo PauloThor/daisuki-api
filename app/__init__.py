7a4cfc2695f549a7e813a4a834603a35133f84f9
Matheus Paiva <matheeus_paiva@hotmail.com>
2021-10-11 12:33:06 -0300

    initial

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from app.configs import database, migration, env_configs, jwt
+from app.routes import api_daisuki
+
+def create_app():
+
+    app = Flask(__name__)
+    env_configs.init_app(app)
+    database.init_app(app)
+    migration.init_app(app)
+    jwt.init_app(app)
+    app.register_blueprint(api_daisuki.bp)
+
+    return app
+
+    
---
+++ app/routes/api_daisuki.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+bp = Blueprint('api_bp', __name__, url_prefix='/daisuki')
+
+
