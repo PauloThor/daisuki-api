e880b81a39aca42d1d1a1730dd7cee86309a4c3b
Laiane Suzart <laiane@pm.me>
2021-10-11 16:26:13 -0300

    Add episode model

---
+++ app/models/episode_model.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from app.configs.database import db
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql.schema import ForeignKey
+from datetime import datetime
+
+@dataclass
+class EpisodeModel(db.Model):
+    id: int
+    anime_id: int
+    episode_number: int
+    image_url: str
+    video_url: str
+    created_at: datetime
+    views: int
+
+    __tablename__ = 'episodes'
+
+    id = Column(Integer, primary_key=True)
+    anime_id = Column(Integer, ForeignKey('animes.id'))
+    episode_number = Column(Integer, nullable=False)
+    image_url = Column(String(255), nullable=False)
+    video_url = Column(String(255), nullable=False)
+    created_at = Column(DateTime(timezone=True), nullable=False)
+    views = Column(Integer, default=0)
