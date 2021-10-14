c64a2c233f95bd5a5ceb36c04e9a8fb4e1ee3638
paulothor <paulothor12345@gmail.com>
2021-10-14 07:48:40 -0300

    fix bp

--- app/routes/episode_blueprint.py
+++ app/routes/episode_blueprint.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from app.controllers.episode_controller import create_episode, get_all_episodes
 
-bp = Blueprint('users', __name__, url_prefix='/episodes')
+bp = Blueprint('episodes', __name__, url_prefix='/episodes')
 
 bp.post('')(create_episode)
 bp.get('')(get_all_episodes)
